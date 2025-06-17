50eaad79041be0e9b9ee344ef7aba40e739c2d9f
Matteo Gambini <m43gambini@gmail.com>
2025-06-17 16:05:39 +0200

    modifica enemy in PIZZA

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (s
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (dottore.vy == 0) {
         dottore.vy = -200
-        music.play(music.melodyPlayable(music.magicWand), music.PlaybackMode.UntilDone)
+        music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.UntilDone)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`miaTessera`, function (sprite, location) {
@@ -164,42 +164,59 @@ dottore = sprites.create(img`
     . . . . . . 1 . . . 1 . . . . . 
     `, SpriteKind.Player)
 myEnemy = sprites.create(img`
-    . . . . . . . e c 7 . . . . . . 
-    . . . . e e e c 7 7 e e . . . . 
-    . . c e e e e c 7 e 2 2 e e . . 
-    . c e e e e e c 6 e e 2 2 2 e . 
-    . c e e e 2 e c c 2 4 5 4 2 e . 
-    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
-    c e e 2 2 f f f 2 f f f 2 2 2 e 
-    c e e 2 2 f f f 2 f f f 2 2 2 e 
-    c e e 2 2 2 f f 2 2 f f 2 2 2 e 
-    c e e 2 f 2 2 2 2 2 2 2 f 4 2 e 
-    . e e e f f 2 f 2 f 2 f f 4 e . 
-    . 2 e e 2 f f f f f f f 4 2 e . 
-    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
-    . . . 2 2 e e 4 4 4 2 e e . . . 
-    . . . . . 2 2 e e e e . . . . . 
+    .............beebbbb............
+    ............eebbbb4bb...........
+    ............eb344bb4bb..........
+    ............e44334bb4bb.........
+    ............eb433344b4be........
+    ............4eb43344444be.......
+    ...........bd4eb43333344bb......
+    ..........b455d4443333444bb.....
+    ..........4d5555d444333444bb....
+    .........4555555dd4b4443444be...
+    ........bd5555d555d4bb444444ee..
+    ........b55ddd665555bb4b44444ee.
+    .......bd5555677655554ebb44444eb
+    .......43222558855555d4eeb44b4ee
+    ......b422332ddd555222d4eebbb4be
+    ......be22232ed55522332db4ebbbbe
+    .....bde22222e555e22232edd4bbbbe
+    .....b52e222e3555e22222eddd4ebee
+    ....bd552eee355552e222e355544eee
+    ....665dd5555555552eee355dd4deee
+    ...6776555555555555555551554d4ee
+    ...4885222555dddd6655551544d4eee
+    ..b45522332555dd677611d444ddeee.
+    ..4d5222232e55555881d44ddd4eee..
+    .bdd5e22222e555115114d54d4ee....
+    .b55d2e222e351144d1d55eeee......
+    bd5ddd2eee3d444555dd4e..........
+    b555115dddd55d544eede...........
+    4511d444d5544ee...4de...........
+    41d4555d4ee........44...........
+    41554eede.......................
+    44ee...4e.......................
     `, SpriteKind.Player)
+myEnemy.setPosition(75, 0)
 controller.moveSprite(dottore, 200, 0)
 dottore.setPosition(19, 19)
 tiles.setCurrentTilemap(tilemap`livello1`)
 scene.cameraFollowSprite(dottore)
 game.setDialogFrame(img`
     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 2 5 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 2 5 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 2 5 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 5 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 2 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    5 5 2 2 2 2 2 2 2 2 2 2 2 5 . 
-    . 5 5 2 2 2 2 2 2 2 2 2 2 5 . 
-    . . . 5 5 5 5 5 2 2 2 2 2 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 1 5 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 1 5 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 1 5 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 5 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 1 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    5 5 1 1 1 1 1 1 1 1 1 1 1 5 . 
+    . 5 5 1 1 1 1 1 1 1 1 1 1 5 . 
+    . . . 5 5 5 5 5 1 1 1 1 1 5 . 
     . . . . . . . 5 5 5 5 5 5 5 . 
     `)
 game.showLongText("questo è un testo incredibilmente lungo e non so come appaia", DialogLayout.Bottom)
